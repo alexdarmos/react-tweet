1b293aa47e80405dfa360333176ca95f9c1bdcb8
Alex Darmos <alex.darmos@gmail.com>
2020-02-19 22:29:45 -0500

    init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function Tweet() {
+	return (
+		<div className='tweet'>
+			<Avatar />
+			<div className='content'>
+				<Author />
+				<Time />
+				<Message />
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+				<div className='buttons'>
+					<ReplyButton />
+					<RetweetButton />
+					<LikeButton />
+					<MoreOptionsButton />
+				</div>
+			</div>
+		</div>
+	);
+}
+
+function Avatar() {
+	return (
+		<img
+			src='https://www.gravatar.com/avatar/nothing'
+			className='avatar'
+			alt='avatar'
+		/>
+	);
+}
+
+function Message() {
+	return <div className='message'>This is less than 140 characters.</div>;
+}
+
+function Author() {
+	return (
+		<span className='author'>
+			<span className='name'>Your Name</span>
+			<span className='handle'>@crypto_cowboy</span>
+		</span>
+	);
+}
+
+const Time = () => <span className='time'>3hr ago</span>;
+
+const ReplyButton = () => <i className='fa fa-reply reply-button' />;
+
+const RetweetButton = () => <i className='fa fa-retweet retweet-button' />;
+
+const LikeButton = () => <i className='fa fa-heart like-button' />;
+
+const MoreOptionsButton = () => (
+	<i className='fa fa-ellipsis-h more-options-button' />
+);
+
+ReactDOM.render(<Tweet />, document.querySelector("#root"));
+
+//pg.60
